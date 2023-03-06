d89865f769626fabebac08c45f807e9604fcdc96
Aziz <afdagli@gmail.com>
2023-03-05 17:24:17 -0800

    initial code moved to github

---
+++ my-app/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function tick() {
+  root.render(<HelloWorld/>);
+}
+
+setInterval(tick, 1000)
+
+
+
+function HelloWorld(props) {
+  const date = new Date().toLocaleTimeString();
+  return (
+    <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {date}.</h2>
+  </div>
+  );
+
+}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
