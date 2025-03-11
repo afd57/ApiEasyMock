83a30db8acd64fb75778ef206399507eb05e944e
isaacBabba <babbaisaac6@outlook.com>
2025-03-11 12:18:14 -0700

    Hello world 2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals1';
+import reportWebVitals from './reportWebVitals2';
+import reportWebVitals from './reportWebVitals3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,6 +18,18 @@ setInterval(tick, 1000)
 
 
 function HelloWorld(props) {
+  const date = new Date().toLocaleTimeString();
+  console.log();
+  return (
+    <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {date}.</h2>
+  </div>
+  );
+
+}
+
+function HelloWorld2(props) {
   const date = new Date().toLocaleTimeString();
   return (
     <div>
